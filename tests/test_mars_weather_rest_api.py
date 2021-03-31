331aed2e52d521a4461d5aaa5fcce14601995a8e
ryanecameron <rcameron@student.bridgew.edu>
2021-03-31 18:56:31 -0400

    Created test for get_mars_data.py

--- tests/test_mars_weather_rest_api.py
+++ tests/test_mars_weather_rest_api.py
@@ -1,10 +1,10 @@
 import pytest
-import get_mars_data
+from get_mars_data import get_data
 import rest_api
 
 
 def test_get_mars_data():
-    result = get_mars_data.get_data()
+    result = get_data()
 
     assert result == 201
     assert result != 404
